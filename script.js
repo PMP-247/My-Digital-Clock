a9acd8e9e3767ddf1b69452859aa5a843ef49fca
PMP-247 <palesa.magolego@icloud.com>
2025-09-29 10:06:05 +0200

    i hope these show up on my repo

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// this code is in a separate file now 
-
+// this code is in a separate file 
 
 let hrs = document.getElementById("hrs")
 let min = document.getElementById("min")
@@ -7,7 +6,7 @@ let sec = document.getElementById("sec")
 
 setInterval(() => {
     let currentTime = new Date(),
-
+//when i tried to declare my hours the entire clock became non functional despite trouble shooting this problem
 hrsinnerHTML = currentTime.getHours();
 min.innerHTML = currentTime.getMinutes(); 
 sec.innerHTML = currentTime.getSeconds();
